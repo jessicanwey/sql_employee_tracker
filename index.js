b1d31b0aa3e07fe4f27b24b5a2e7d9a9450e1d2e
jessicanwey <jessica.n.wey@gmail.com>
2024-01-04 16:52:38 -0500

    SQL for view roles and departments working

--- index.js
+++ index.js
@@ -22,22 +22,72 @@ const db = mysql.createConnection(
   console.log(`Connected to the staff_db database.`)
 );
 
-inquirer.prompt([
-    {
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'action',
-        choices: [
-            'View All Departments',
-            'View All Roles',
-            'View All Employees',
-            'Add a Department',
-            'Add a Role',
-            'Add an Employee',
-            'Update an Employee Role'
-        ]
-    },
-])
-.then((response) =>
-    console.log(response.action)
-);
+const promptUser = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'action',
+            choices: [
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add a Department',
+                'Add a Role',
+                'Add an Employee',
+                'Update an Employee Role'
+            ]
+        },
+    ])
+    .then((response) => {
+        console.log(response.action);
+        //viewAllDepartments();
+        viewAllRoles();
+        //viewAllEmployees();
+        //addDepartment();
+        //addRole();
+        //addEmployee();
+        //updateEmployee();
+    });
+}
+
+promptUser();
+
+const viewAllDepartments = () => {
+    db.query('SELECT id AS ID, name AS Department FROM departments;', function (err, results){
+        console.log(err);
+        console.table(results);
+        promptUser();
+    })
+}
+
+const viewAllRoles = () => {
+    const viewRolesSQL = 'SELECT roles.id as ID, roles.title as Title, departments.name AS Department, roles.salary AS Salary FROM roles JOIN departments ON roles.department_id = departments.id;'
+    db.query(viewRolesSQL, function (err, results){
+        console.table(results);
+        promptUser();
+    })
+}
+
+const viewAllEmployees = () => {
+    db.query('SELECT * FROM employees', function (err, results){
+        console.table(results);
+        promptUser();
+    })
+}
+
+const addDepartment = () => {
+    
+}
+
+const addRole = () => {
+
+}
+
+const addEmployee = () => {
+
+}
+
+const updateEmployee = () => {
+
+}
